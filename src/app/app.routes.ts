66866c43752e69048c8cf23c77381ea3fee4a2b0
lucia aguirre <guerrolucia@gmail.com>
2024-08-05 18:46:59 +0200

    fix: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,38 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
 export const APP_ROUTES: Routes = [
   {
-    path: 'teas',
+    path: "teas",
     loadComponent: () =>
-      import('./pages/home.component').then((c) => c.HomeComponent),
-    children: [],
+      import("./pages/home.component").then((c) => c.HomeComponent),
+    children: [
+      {
+        path: "",
+        loadComponent: () =>
+          import("./pages/tea-list/tea-list.component").then(
+            (c) => c.TeaListComponent
+          ),
+        canActivate: [authGuard],
+      },
+      {
+        path: ":id",
+        loadComponent: () =>
+          import("./pages/tea-detail/tea-detail.component").then(
+            (c) => c.TeaDetailComponent
+          ),
+        canActivate: [authGuard],
+        resolve: { tea: TeaResolver },
+      },
+    ],
   },
   {
-    path: 'auth',
-    loadChildren: () => import('./auth/auth.routes').then((m) => m.routes),
-    // canActivate: [isSignIn],
+    path: "auth",
+    loadChildren: () => import("./auth/auth.routes").then((m) => m.routes),
+    canActivate: [isSignIn],
   },
   {
-    path: '**',
-    redirectTo: 'teas',
-    pathMatch: 'full',
+    path: "**",
+    redirectTo: "teas",
+    pathMatch: "full",
   },
 ];
