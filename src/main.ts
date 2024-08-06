de22f96f51c450d498715530e29000c473acdd6b
lucia aguirre <guerrolucia@gmail.com>
2024-08-06 07:42:04 +0200

    git: adds app config to fix httpClient injector

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,5 @@
 import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
-import { provideRouter } from "@angular/router";
-import { APP_ROUTES } from "./app/app.routes";
-import { provideHttpClient } from "@angular/common/http";
-import { CustomHttpInterceptor } from "./app/interceptors/custom-http.interceptor";
-import { provideAnimations } from "@angular/platform-browser/animations";
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(APP_ROUTES),
-    provideHttpClient(withInterceptors([CustomHttpInterceptor])),
-    provideAnimations(),
-  ],
-});
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.log(err));
